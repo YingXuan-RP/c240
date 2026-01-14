defdb48455df95c9cb480d43b334bf98d7cd735c
itzmexuan06 <23023432@myrp.edu.sg>
2026-01-14 22:01:08 +0800

    Update query function

--- js/app.js
+++ js/app.js
@@ -421,7 +421,7 @@
   };
 
   // Flowise API integration
-  async function query(data) {
+ async function query(data) {
     const response = await fetch(
         "https://cloud.flowiseai.com/api/v1/prediction/4919fadf-112c-4393-93be-0eb7b16b1c40",
         {
@@ -434,11 +434,11 @@
     );
     const result = await response.json();
     return result;
-  }
+}
 
-  query({"question": "Hey, how are you?"}).then((response) => {
+query({"question": "Hey, how are you?"}).then((response) => {
     console.log(response);
-  });
+});
 
   // Chatbot intents and responses
   const chatbotIntents = [
